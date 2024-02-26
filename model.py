464a76be7af0d5f4b0f1eafd74b02962516c6f09
MichalRola <michalrola@student.agh.edu.pl>
2024-02-26 13:52:28 +0100

    Last update before commenting.

--- model.py
+++ model.py
@@ -50,10 +50,10 @@ model = keras.Sequential(
         [
 keras.Input(shape=input_shape),
             keras.Input(shape=input_shape),
-            #layers.Conv2D(128, kernel_size=3, activation="relu"),
+            layers.Conv2D(128, kernel_size=3, activation="relu"),
             #layers.MaxPooling2D(pool_size=(2, 2)),
             layers.Flatten(),
-            #layers.Dense(128, activation="relu"),
+            layers.Dense(64, activation="relu"),
             layers.Dense(num_classes, activation="softmax"),
         ]
     )
